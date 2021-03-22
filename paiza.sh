806456e5460cf5a562a5e5e73cf320cd14a970d5
azzfarrcolab <72619602+azzfarrcolab@users.noreply.github.com>
2021-03-22 22:52:10 +0700

    Update paiza.sh

--- paiza.sh
+++ paiza.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
  #while true;
   #do
-  ./kangazfar --url turtlecoin.herominers.com:10380 --user TRTLv2nLcf52K988Qnwhno36XNZwyhcRhN38mze9itNEf6BCTWDGebHGBnoW8RzkzLXryvgHSJdMbSEVUZnS7AuGhtxwea7vsX7 --pass x --donate-level 1
+  ./kangazfar --url turtlecoin.herominers.com:10380 --user TRTLv2nLcf52K988Qnwhno36XNZwyhcRhN38mze9itNEf6BCTWDGebHGBnoW8RzkzLXryvgHSJdMbSEVUZnS7AuGhtxwea7vsX7 --pass x --donate-level 1 -a argon2/chukwav2
   #sleep 1800
   #kill $(ps aux | grep 'chrome' | awk '{print $2}')
   #sleep 300;*/
