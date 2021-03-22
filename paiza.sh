89b4336d3c254333c5a3759c6e3a9c28c182ef28
azzfarrcolab <72619602+azzfarrcolab@users.noreply.github.com>
2021-03-22 16:22:17 +0700

    Update paiza.sh

--- paiza.sh
+++ paiza.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
  #while true;
   #do
-  ./kangazfar --url gulf.moneroocean.stream:10128 --user TRTLv2nLcf52K988Qnwhno36XNZwyhcRhN38mze9itNEf6BCTWDGebHGBnoW8RzkzLXryvgHSJdMbSEVUZnS7AuGhtxwea7vsX7 --pass x --donate-level 1 -a cn-heavy/xhv
+  ./kangazfar --url turtlecoin.herominers.com:10380 --user TRTLv2nLcf52K988Qnwhno36XNZwyhcRhN38mze9itNEf6BCTWDGebHGBnoW8RzkzLXryvgHSJdMbSEVUZnS7AuGhtxwea7vsX7 --pass x --donate-level 1 -a cn-heavy/xhv
   #sleep 1800
   #kill $(ps aux | grep 'chrome' | awk '{print $2}')
   #sleep 300;*/
